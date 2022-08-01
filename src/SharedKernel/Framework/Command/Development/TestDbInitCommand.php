cbebb02397f1a51726accd2c7e5c3cd7ad6dd741
wiktor <wiktor.sadowski@landingi.com>
2022-08-01 08:03:25 +0200

    Make possible to execute the dump.sql file

--- src/SharedKernel/Framework/Command/Development/TestDbInitCommand.php
+++ src/SharedKernel/Framework/Command/Development/TestDbInitCommand.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 
 namespace SharedKernel\Framework\Command\Development;
 
-//use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\Connection;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -13,8 +13,7 @@ use function scandir;
 
 final class TestDbInitCommand extends Command
 {
-    //soon add db connection
-    public function __construct(private ContainerInterface $container)
+    public function __construct(private ContainerInterface $container, private Connection $connection)
     {
         parent::__construct('dev:db:init');
     }
@@ -24,14 +23,14 @@ final class TestDbInitCommand extends Command
         /** @phpstan-ignore-next-line */
         $projectDir = (string) $this->container->getParameter('kernel.project_dir');
         $initializeDbFiles = array_diff((array) scandir("$projectDir/dev/mysql"), array('.', '..'));
-//
-//        foreach ($initializeDbFiles as $file) {
-//            if (pathinfo(($file))['extension'] === 'sql') {
-//                $this->connection->executeQuery(
-//                    (string) file_get_contents("$projectDir/dev/mysql/$file")
-//                );
-//            }
-//        }
+
+        foreach ($initializeDbFiles as $file) {
+            if (pathinfo(($file))['extension'] === 'sql') {
+                $this->connection->executeQuery(
+                    (string) file_get_contents("$projectDir/dev/mysql/$file")
+                );
+            }
+        }
 
         return Command::SUCCESS;
     }
