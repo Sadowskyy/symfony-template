ac65528ae3311ff7f07394170310cf7426f6db26
wiktor <wiktor.sadowski@landingi.com>
2022-08-01 04:32:23 +0200

    Created console command injecting into the app the data dump.

---
+++ src/SharedKernel/Framework/Command/Development/TestDbInitCommand.php
@@ -0,0 +1,35 @@
+<?php
+declare(strict_types=1);
+
+namespace SharedKernel\Framework\Command\Development;
+
+//use Doctrine\DBAL\Connection;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+final class TestDbInitCommand extends Command
+{
+    //soon add db connection
+    public function __construct(private ContainerInterface $container)
+    {
+        parent::__construct('dev:db:init');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+//        $projectDir = $this->container->getParameter('kernel.project_dir');
+//        $initializeDbFiles = array_diff(scandir("$projectDir/dev/mysql"), array('.', '..'));
+//
+//        foreach ($initializeDbFiles as $file) {
+//            if (pathinfo(($file))['extension'] === 'sql') {
+//                $this->connection->executeQuery(
+//                    (string) file_get_contents("$projectDir/dev/mysql/$file")
+//                );
+//            }
+//        }
+
+        return 0;
+    }
+}
