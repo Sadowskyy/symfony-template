c63ba74d549b7466bfea89beca862464f12a5bfd
Dawid Zaręba <68419282+Fejren@users.noreply.github.com>
2022-08-05 22:14:50 +0200

    Update dump.sql

--- dev/mysql/dump.sql
+++ dev/mysql/dump.sql
@@ -1,1 +1,10 @@
--- You can paste here some schemas etc. also you can do it in another file with .sql extension
+-- You can paste here some schemas etc. also you can do it in anotheCREATE TABLE User (
+CREATE TABLE User (
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL DEFAULT '',
+    username VARCHAR(255) NOT NULL DEFAULT '',
+    password VARCHAR(255) NOT NULL,
+    is_active BOOLEAN NOT NULL, -- For development is_active is not nullable
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
