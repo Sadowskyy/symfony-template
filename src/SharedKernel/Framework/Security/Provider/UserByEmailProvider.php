4e9b5cf468cdc9b12ed008d1ac65ab900b9951e8
wiktor <wiktor.sadowski@landingi.com>
2022-08-07 01:13:08 +0200

    error fix

--- src/SharedKernel/Framework/Security/Provider/UserByEmailProvider.php
+++ src/SharedKernel/Framework/Security/Provider/UserByEmailProvider.php
@@ -11,15 +11,28 @@ use SharedKernel\Domain\PasswordHash;
 use SharedKernel\Framework\Security\User\SharedUser;
 use SharedKernel\Framework\Security\User\UserData;
 use Symfony\Bridge\Doctrine\Security\User\UserLoaderInterface;
+use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
+use Symfony\Component\Security\Core\Exception\UserNotFoundException;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
 use Symfony\Component\Uid\Uuid;
 
-final class UserByEmailProvider implements UserLoaderInterface
+final class UserByEmailProvider implements UserProviderInterface
 {
+
     public function __construct(private Connection $connection) {
     }
 
-    public function loadUserByIdentifier(string $identifier): ?UserInterface
+    public function refreshUser(UserInterface $user)
+    {
+    }
+
+    public function supportsClass(string $class): bool
+    {
+        return SharedUser::class === $class;
+    }
+
+    public function loadUserByIdentifier(string $identifier): UserInterface
     {
         $user = $this->connection->fetchAssociative((string) new GetUserByEmailQuery(), [$identifier]);
 
@@ -34,6 +47,5 @@ final class UserByEmailProvider implements UserLoaderInterface
             new PasswordHash($user['password']),
         );
 
-        return new SharedUser($userData);
-    }
+        return new SharedUser($userData);    }
 }
