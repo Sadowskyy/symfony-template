65334dc4cb76f330c093c266d1073315cc3211ec
wiktor <wiktor.sadowski@landingi.com>
2022-08-07 01:28:25 +0200

    ecs simple config

---
+++ ecs.php
@@ -0,0 +1,29 @@
+<?php
+use Symplify\CodingStandard\Fixer\Annotation\DoctrineAnnotationNestedBracketsFixer;
+use Symplify\CodingStandard\Fixer\LineLength\LineLengthFixer;
+use Symplify\EasyCodingStandard\Config\ECSConfig;
+use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
+
+return static function (ECSConfig $ecsConfig): void {
+    $ecsConfig->rule(LineLengthFixer::class);
+
+    $ecsConfig->ruleWithConfiguration(DoctrineAnnotationNestedBracketsFixer::class, [
+        DoctrineAnnotationNestedBracketsFixer::ANNOTATION_CLASSES => ['Doctrine\ORM\JoinColumns'],
+    ]);
+
+    $ecsConfig->sets([
+        SetList::CLEAN_CODE,
+        SetList::SYMPLIFY,
+        SetList::COMMON,
+        SetList::PSR_12,
+        SetList::DOCTRINE_ANNOTATIONS,
+    ]);
+
+    $ecsConfig->paths([
+        __DIR__ . '/packages',
+        __DIR__ . '/tests',
+        __DIR__ . '/ecs.php',
+        __DIR__ . '/monorepo-builder.php',
+        __DIR__ . '/rector.php',
+    ]);
+};
