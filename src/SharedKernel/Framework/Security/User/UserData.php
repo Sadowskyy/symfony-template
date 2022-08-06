2aa88cdb86aa61472302aec32b336d383c4025d7
Wiktor Sadowski <58751887+Sadowskyy@users.noreply.github.com>
2022-08-07 00:45:57 +0200

    Created framework User class. (#20)
    Created user class which can be used everywhere in application.

---
+++ src/SharedKernel/Framework/Security/User/UserData.php
@@ -0,0 +1,39 @@
+<?php
+declare(strict_types=1);
+
+namespace SharedKernel\Framework\Security\User;
+
+use SharedKernel\Domain\Email;
+use SharedKernel\Domain\PasswordHash;
+use Symfony\Component\Uid\Uuid;
+
+final class UserData
+{
+    public function __construct(
+        private int $userId,
+        private Uuid $userUuid,
+        private Email $email,
+        private PasswordHash $passwordHash
+    ) {
+    }
+
+    public function getUserId(): int
+    {
+        return $this->userId;
+    }
+
+    public function getUserUuid(): Uuid
+    {
+        return $this->userUuid;
+    }
+
+    public function getEmail(): Email
+    {
+        return $this->email;
+    }
+
+    public function getPasswordHash(): PasswordHash
+    {
+        return $this->passwordHash;
+    }
+}
