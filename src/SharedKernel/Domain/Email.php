832b0820f6768c4c2f3001c3563dde6d473bf217
wiktor <wiktor.sadowski@landingi.com>
2022-08-07 01:32:01 +0200

    ecs fix

--- src/SharedKernel/Domain/Email.php
+++ src/SharedKernel/Domain/Email.php
@@ -1,4 +1,5 @@
 <?php
+
 declare(strict_types=1);
 
 namespace SharedKernel\Domain;
@@ -15,17 +16,17 @@ final class Email implements JsonSerializable
         $address = trim($address, " \t\n\r\0\x0B`");
         $this->address = $address;
 
-        if (!filter_var($address, FILTER_VALIDATE_EMAIL)) {
-            throw new InvalidEmailException("$address is not a valid email");
+        if (! filter_var($address, FILTER_VALIDATE_EMAIL)) {
+            throw new InvalidEmailException("{$address} is not a valid email");
         }
     }
 
-    public function jsonSerialize(): string
+    public function __toString(): string
     {
         return $this->address;
     }
 
-    public function __toString(): string
+    public function jsonSerialize(): string
     {
         return $this->address;
     }
