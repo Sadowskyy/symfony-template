692772ec7e9b033455eb6efe8a6190a8018b9d7a
wiktor <wiktor.sadowski@landingi.com>
2022-08-07 01:09:42 +0200

    Created user by email provider

---
+++ src/SharedKernel/Framework/Security/Provider/UserByEmailProvider.php
@@ -0,0 +1,39 @@
+<?php
+declare(strict_types=1);
+
+namespace SharedKernel\Framework\Security\Provider;
+
+use Doctrine\DBAL\Connection;
+use Domain\Exception\UserCannotBeFound;
+use SharedKernel\Application\Query\User\GetUserByEmailQuery;
+use SharedKernel\Domain\Email;
+use SharedKernel\Domain\PasswordHash;
+use SharedKernel\Framework\Security\User\SharedUser;
+use SharedKernel\Framework\Security\User\UserData;
+use Symfony\Bridge\Doctrine\Security\User\UserLoaderInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Uid\Uuid;
+
+final class UserByEmailProvider implements UserLoaderInterface
+{
+    public function __construct(private Connection $connection) {
+    }
+
+    public function loadUserByIdentifier(string $identifier): ?UserInterface
+    {
+        $user = $this->connection->fetchAssociative((string) new GetUserByEmailQuery(), [$identifier]);
+
+        if (empty($user)) {
+            throw new UserCannotBeFound("User with email ($identifier) cannot be found");
+        }
+
+        $userData = new UserData(
+            (int)$user['id'],
+            Uuid::fromString($user['uuid']),
+            new Email($user['email']),
+            new PasswordHash($user['password']),
+        );
+
+        return new SharedUser($userData);
+    }
+}
