d2c00904ba04527a1785d3d3a4dba2534cc37070
Wiktor Sadowski <58751887+Sadowskyy@users.noreply.github.com>
2022-08-10 14:46:00 +0200

    Login form - login via form (#23)
    Here I got added form login authorization. Next will be probably API Token.

--- src/SharedKernel/Framework/Security/User/SharedUser.php
+++ src/SharedKernel/Framework/Security/User/SharedUser.php
@@ -4,9 +4,10 @@ declare(strict_types=1);
 
 namespace SharedKernel\Framework\Security\User;
 
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
-class SharedUser implements UserInterface
+class SharedUser implements UserInterface, PasswordAuthenticatedUserInterface
 {
     public function __construct(private UserData $userData)
     {
