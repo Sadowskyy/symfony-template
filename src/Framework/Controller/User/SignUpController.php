8e08b94ab57613f184bc8a294e970e386151c4b6
Wiktor Sadowski <58751887+Sadowskyy@users.noreply.github.com>
2022-11-30 23:52:12 +0100

    Registered sign up controller with empty logic.

---
+++ src/Framework/Controller/User/SignUpController.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Framework\Controller\User;
+
+use Framework\Form\User\SignUpForm;
+use Framework\Response\Redirect;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SignUpController extends AbstractController
+{
+    #[Route('/register', name: 'REGISTER_USER', methods: ['GET', 'POST'])]
+    public function registerUser(Request $request): Response
+    {
+        $form = $this->createForm(SignUpForm::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+
+            return new Redirect('/login');
+        }
+
+        return $this->render('user/registration.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+}
