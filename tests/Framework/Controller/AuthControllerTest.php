74660291fb3bd0375d52dfc8742e3c81735ef9a9
Wiktor Sadowski <58751887+Sadowskyy@users.noreply.github.com>
2022-12-03 22:49:37 +0100

    Added AuthControllerTest

---
+++ tests/Framework/Controller/AuthControllerTest.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Framework\Controller;
+
+use Doctrine\DBAL\Driver\Connection;
+use SharedKernel\Application\Query\User\GetUserByEmailQuery;
+use Tests\Utils\AbstractFunctionalTestCase;
+
+class AuthControllerTest extends AbstractFunctionalTestCase
+{
+    private Connection $connection;
+    
+    public function __construct(Connection $connection)
+    {
+        parent::__construct(null, [], '$dataName');
+    }
+
+    public function testMainPageEndpoint(): void
+    {
+        $client = $this->getClient();
+        $user = $this->connection->exec((string) new GetUserByEmailQuery());
+        dd($user);
+    }
+}
